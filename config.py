fd08a8c7f5b5d08d002d9eb521aa71e8606f2916
ajadprajapati224155-prog <ajadprajapati224155@gmail.com>
2026-05-28 10:50:18 +0530

    Set default values for API and database configurations
    Updated environment variable defaults for deployment.

--- config.py
+++ config.py
@@ -3,10 +3,11 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", ""))
-MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "")
-LOG_GROUP = int(getenv("LOG_GROUP", ""))
-FORCESUB = getenv("FORCESUB", "")
+API_ID = int(getenv("API_ID", "35279304"))
+API_HASH = getenv("API_HASH", "49ea7646f4251b3ca5a7798c61bb5e9f")
+BOT_TOKEN = getenv("BOT_TOKEN", "8865573129:AAEJHbTopdW1JTtlxai7zl9_GFYfX0glR7o")
+OWNER_ID = int(getenv("OWNER_ID", "6824252172"))
+MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://privatefiles224155_db_user:mypassword123@cluster0.nrephhv.mongodb.net/?appName=Cluster0")
+LOG_GROUP = int(getenv("LOG_GROUP", "-1004291498203"))
+FORCESUB = getenv("FORCESUB", "-1002718563674
+")
